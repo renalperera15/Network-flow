d98e45129ad487529347181f4f25511ab8ee6113
Renal Perera <renalperera15@gmail.com>
2025-04-16 23:18:50 +0530

    Added the edge code

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,20 @@
 public class Edge {
+    int from, to, capacity, flow;
+    Edge residual;
+
+    public Edge(int from, int to, int capacity) {
+        this.from = from;
+        this.to = to;
+        this.capacity = capacity;
+        this.flow = 0;
+    }
+
+    public int remainingCapacity() {
+        return capacity - flow;
+    }
+
+    public void augment(int bottleNeck) {
+        flow += bottleNeck;
+        residual.flow -= bottleNeck;
+    }
 }
